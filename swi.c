a897e72bd2a089de453c03e346600b045518ce08
C-Parimala <30308286+C-Parimala@users.noreply.github.com>
2017-07-19 23:20:22 -1200

    Create swi.c

---
+++ swi.c
@@ -0,0 +1,29 @@
+#include<stdio.h>
+
+int main() {
+    char n;
+  char a[10];
+  scanf("%c",&n);
+  switch(n)
+  {
+      case 'a':
+      printf("vowel");
+      break;
+      case 'e':
+      printf("vowel");
+      break;
+      case 'i':
+      printf("vowel");
+      break;
+      case 'u':
+      printf("vowel");
+      break;
+      case 'o':
+      printf("vowel");
+      break;
+      default:
+      printf("constant");
+      break;
+  }
+  return 0;
+  }
