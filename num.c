5c13cb034a8ccc58f30a97c94d5ec2aa8c4e2287
C-Parimala <30308286+C-Parimala@users.noreply.github.com>
2017-07-19 22:45:08 -1200

    Create num.c

---
+++ num.c
@@ -0,0 +1,19 @@
+#include<stdio.h>
+int main()
+{
+int n;
+scanf("%d",n);
+if(n==0)
+{
+printf("the num is zero");
+}
+if(n>0)
+{
+printf("the num is +ve");
+}
+if(n<0)
+{
+printf("the num is -ve");
+}
+return 0;
+}
